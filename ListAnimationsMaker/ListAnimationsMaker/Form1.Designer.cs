a268c59eda368b0f5d42810a5f5d0f2ee855d87b
Brandon Lee <brandononlyu@gmail.com>
2017-07-18 15:44:51 -0400

    Added all files

---
+++ ListAnimationsMaker/ListAnimationsMaker/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ListAnimationsMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.loadBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.generate = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.Location = new System.Drawing.Point(205, 200);
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(75, 23);
+            this.loadBtn.TabIndex = 0;
+            this.loadBtn.Text = "Load...";
+            this.loadBtn.UseVisualStyleBackColor = true;
+            this.loadBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 167);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(460, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // generate
+            // 
+            this.generate.Location = new System.Drawing.Point(205, 229);
+            this.generate.Name = "generate";
+            this.generate.Size = new System.Drawing.Size(75, 23);
+            this.generate.TabIndex = 2;
+            this.generate.Text = "Generate";
+            this.generate.UseVisualStyleBackColor = true;
+            this.generate.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Curlz MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(342, 46);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "List Animations Maker";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Curlz MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(352, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 21);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "by Brandon Lee";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.generate);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.loadBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "List Animations Maker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button generate;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ListAnimationsMaker/ListAnimationsMaker/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ListAnimationsMaker
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap bmp;
+        public bool loaded = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Load Button
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Portable Network Graphics|*.PNG;";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(ofd.FileName);
+                Image i = Image.FromStream(sr.BaseStream);
+                bmp = new Bitmap(i);
+                loaded = true;
+                if (bmp.Width%16 != 0 || bmp.Height%9 != 0)
+                {
+                    MessageBox.Show("Image is not 16x9. Image is unable to play in List Animations",
+                        "Image Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    sr.Close();
+                    loaded = false;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            if (loaded)
+            {
+                string outStr = "";
+                string hex = "";
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        if (bmp.GetPixel(x, y) == Color.FromArgb(255, 0, 0, 0))
+                        {
+                            outStr += "1";
+                        }
+                        else if (bmp.GetPixel(x, y) == Color.FromArgb(0, 0, 0, 0))
+                        {
+                            outStr += "0";
+                        }
+                        else
+                        {
+                            MessageBox.Show("An invalid color (" + bmp.GetPixel(x, y).ToString() + ") was detected. Please use black or transparent.",
+                                "Image Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                            break;
+                        }
+                    }
+                }
+                for (int i = 0; i < outStr.Length / 4; i++)
+                {
+                    switch (outStr.Substring(i * 4, 4))
+                    {
+                        case "0000":
+                            hex += "0";
+                            break;
+                        case "0001":
+                            hex += "1";
+                            break;
+                        case "0010":
+                            hex += "2";
+                            break;
+                        case "0011":
+                            hex += "3";
+                            break;
+                        case "0100":
+                            hex += "4";
+                            break;
+                        case "0101":
+                            hex += "5";
+                            break;
+                        case "0110":
+                            hex += "6";
+                            break;
+                        case "0111":
+                            hex += "7";
+                            break;
+                        case "1000":
+                            hex += "8";
+                            break;
+                        case "1001":
+                            hex += "9";
+                            break;
+                        case "1010":
+                            hex += "A";
+                            break;
+                        case "1011":
+                            hex += "B";
+                            break;
+                        case "1100":
+                            hex += "C";
+                            break;
+                        case "1101":
+                            hex += "D";
+                            break;
+                        case "1110":
+                            hex += "E";
+                            break;
+                        case "1111":
+                            hex += "F";
+                            break;
+                        default:
+                            MessageBox.Show("Bin to Hex conversion failed.", "Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                    }
+                }
+                textBox1.Text = hex;
+                System.GC.Collect();
+            } else
+            {
+                MessageBox.Show("Please load an image first.", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://scratch.mit.edu/projects/168707761/#player");
+        }
+    }
+}
